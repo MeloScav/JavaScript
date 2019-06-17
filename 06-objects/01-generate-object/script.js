18ceaaede2c9a7a955f81a3626bd18a6b9b955c6
MeloScav <meloscavezzoni@gmail.com>
2019-06-17 08:10:23 +0200

    objet 1

--- 06-objects/01-generate-object/script.js
+++ 06-objects/01-generate-object/script.js
@@ -10,5 +10,24 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    document.getElementById("run").addEventListener("click", ()=>{
+        let presentation = {
+            lastname : "Scavezzoni",      // propriété
+            firstname : "Méloé",
+            age : 24,
+            city: "Trooz",
+            country : "Belgique",
+
+            identite : function(){                                     // méthode
+                return "Nom de famille : " + this.lastname +
+                       "\nPrénom : " + this.firstname +
+                       "\nÂge : " + this.age +
+                       "\nVille : " + this.city +
+                       "\nPays : " + this.country;
+            }
+        }
+        console.log(presentation.identite());
+    });
+
+
 })();
